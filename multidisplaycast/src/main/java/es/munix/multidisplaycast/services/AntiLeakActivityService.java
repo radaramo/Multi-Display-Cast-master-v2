274643209db888736c21ef675135570627580d09
Raul <raul_ramirez12@outlook.com>
2018-09-01 14:30:04 -0500

    Initial commit

---
+++ multidisplaycast/src/main/java/es/munix/multidisplaycast/services/AntiLeakActivityService.java
@@ -0,0 +1,28 @@
+package es.munix.multidisplaycast.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import es.munix.multidisplaycast.CastManager;
+
+/**
+ * Created by munix on 2/11/16.
+ */
+
+public class AntiLeakActivityService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind( Intent intent ) {
+        return null;
+    }
+
+    @Override
+    public void onTaskRemoved( Intent rootIntent ) {
+        CastManager.getInstance().onDestroy();
+        stopSelf();
+        super.onTaskRemoved( rootIntent );
+    }
+}
